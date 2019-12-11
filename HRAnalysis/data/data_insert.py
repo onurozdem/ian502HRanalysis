d94807afcf7a6bbb234b9270b20dfd30b52881a3
onurozdemir <onurozdemir94@hotmail.com>
2019-12-12 01:01:01 +0300

    model added and migrated. initial data loaded.

---
+++ HRAnalysis/data/data_insert.py
@@ -0,0 +1,24 @@
+from HRAnalysis.models import UnprocessedData
+import pandas as pd
+import json
+
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+insert_data = pd.read_csv(BASE_DIR + "/data/HR_data.csv")
+insert_data = pd.read_csv("C:\\Users\\onuro\\Desktop\\ŞehirÜni\\VeriOdaklıProgramlama\\Gitproject\\ian502HRanalysis\\HRAnalysis\\data\\HR_data.csv")
+insert_data_dict = insert_data.to_dict('records')
+
+i = 0
+for row in insert_data_dict:
+    row['id'] = i
+    i += 1
+    m = UnprocessedData(**row)
+    m.save()
+
+json = json.dumps(insert_data_dict)
+f = open("data.json","w")
+f.write(json)
+f.close()
--- HRAnalysis/models.py
+++ HRAnalysis/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class UnprocessedData(models.Model):
+    Age = models.IntegerField()
+    Attrition = models.CharField(max_length=200)
+    BusinessTravel = models.CharField(max_length=200)
+    DailyRate = models.IntegerField()
+    Department = models.CharField(max_length=200)
+    DistanceFromHome = models.IntegerField()
+    Education = models.IntegerField()
+    EducationField = models.CharField(max_length=200)
+    EmployeeCount = models.IntegerField()
+    EmployeeNumber = models.IntegerField()
+    EnvironmentSatisfaction = models.IntegerField()
+    Gender = models.CharField(max_length=200)
+    HourlyRate = models.IntegerField()
+    JobInvolvement = models.IntegerField()
+    JobLevel = models.IntegerField()
+    JobRole = models.CharField(max_length=200)
+    JobSatisfaction = models.IntegerField()
+    MaritalStatus = models.CharField(max_length=200)
+    MonthlyIncome = models.IntegerField()
+    MonthlyRate = models.IntegerField()
+    NumCompaniesWorked = models.IntegerField()
+    Over18 = models.CharField(max_length=200)
+    OverTime = models.CharField(max_length=200)
+    PercentSalaryHike = models.IntegerField()
+    PerformanceRating = models.IntegerField()
+    RelationshipSatisfaction = models.IntegerField()
+    StandardHours = models.IntegerField()
+    StockOptionLevel = models.IntegerField()
+    TotalWorkingYears = models.IntegerField()
+    TrainingTimesLastYear = models.IntegerField()
+    WorkLifeBalance = models.IntegerField()
+    YearsAtCompany = models.IntegerField()
+    YearsInCurrentRole = models.IntegerField()
+    YearsSinceLastPromotion = models.IntegerField()
+    YearsWithCurrManager = models.IntegerField()
+
+
+
+
+#class ModelDetail(models.Model):
+#    pass
+
+"""class Item(models.Model):
+	todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+	text = models.CharField(max_length=300)
+	complete = models.BooleanField()
+
+	def __str__(self):
+		return self.text"""
